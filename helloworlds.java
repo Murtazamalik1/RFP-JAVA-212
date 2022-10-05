9fed0d5fcf3bb5ac549b82a3b9f395fca85002eb
murtaza <murtazamalik1319@gmail.com>
2022-10-05 19:11:33 +0530

    this is a simple java program

---
+++ helloworlds.java
@@ -0,0 +1,11 @@
+public class helloworld {
+    public static void main(String[] args) {
+         while (true) {
+            System.out.println("hello malik");
+            try  {
+                Thread.sleep(10000);
+            }  catch (Exception ex) {}
+
+         }
+    }
+}
